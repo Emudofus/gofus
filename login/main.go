7a86ddc10c5291974b34ed9fc62589881d5a7164
Blackrush <blackrushx@gmail.com>
2013-08-31 13:32:23 +0200

    adds flags and parses them

--- login/main.go
+++ login/main.go
@@ -15,6 +15,10 @@ var (
 	lport     = flag.Int("lport", 5555, "the port the login server will listen on")
 	rport     = flag.Int("rport", 5554, "the port the realm server will listen on")
 	nbWorkers = flag.Int("nbWorkers", 1, "the number of workers to start")
+
+	dbuser = flag.String("dbuser", "postgres", "the username used to connect to the PostgreSQL database")
+	dbname = flag.String("dbname", "gofus", "the name of the PostgreSQL database")
+	dbpass = flag.String("dbpass", "", "the password used to connect to the PostgreSQL database")
 )
 
 func wait_user_input() <-chan os.Signal {
@@ -24,6 +28,7 @@ func wait_user_input() <-chan os.Signal {
 }
 
 func main() {
+	flag.Parse()
 	fmt.Println(` _______  _______  _______           _______           .-.        .-.           
 (  ____ \(  ___  )(  ____ \|\     /|(  ____ \          : :        : :           
 | (    \/| (   ) || (    \/| )   ( || (    \/    .--.  : :  .---. : -..  .--.   
@@ -36,7 +41,7 @@ func main() {
 
 	database := db.Open(&db.Configuration{
 		Driver:         "postgres",
-		DataSourceName: "user=postgres dbname=gofus password=bla sslmode=disable",
+		DataSourceName: fmt.Sprint("user=%s dbname=%s password=%s sslmode=disable", *dbuser, *dbname, *dbpass),
 	})
 	defer database.Close()
 
