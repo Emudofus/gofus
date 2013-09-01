00cfe30479a5a570ee9262dc170750da26d2c645
Blackrush <blackrushx@gmail.com>
2013-09-01 23:50:28 +0200

    map opcode to frontend messages

--- protocol/frontend/msg/msg.go
+++ protocol/frontend/msg/msg.go
@@ -1,9 +1,29 @@
 package msg
 
+import (
+	protocol "github.com/Blackrush/gofus/protocol/frontend"
+)
+
 // Converts a boolean value to an integer (1 if true or 0 if false)
 func btoi(b bool) int {
 	if b {
 		return 1
 	}
 	return 0
+}
+
+var producers = make(map[string]func() protocol.MessageContainer)
+
+func init() {
+	// adds only that are received
+	producers["Af"] = func() protocol.MessageContainer { return new(QueueStatusRequest) }
+	producers["AX"] = func() protocol.MessageContainer { return new(RealmServerSelectionRequest) }
+	producers["AT"] = func() protocol.MessageContainer { return new(RealmLoginReq) }
+}
+
+func New(opcode string) (protocol.MessageContainer, bool) {
+	if producer, ok := producers[opcode]; ok {
+		return producer(), true
+	}
+	return nil, false
 }
--- protocol/frontend/msg/realm.go
+++ protocol/frontend/msg/realm.go
@@ -11,16 +11,16 @@ func (msg *HelloGame) Opcode() string                 { return "HG" }
 func (msg *HelloGame) Serialize(out io.Writer) error  { return nil }
 func (msg *HelloGame) Deserialize(in io.Reader) error { return nil }
 
-type RealmLogin struct {
+type RealmLoginReq struct {
 	Ticket string
 }
 
-func (msg *RealmLogin) Opcode() string { return "AT" }
-func (msg *RealmLogin) Serialize(out io.Writer) error {
+func (msg *RealmLoginReq) Opcode() string { return "AT" }
+func (msg *RealmLoginReq) Serialize(out io.Writer) error {
 	fmt.Fprint(out, msg.Ticket)
 	return nil
 }
-func (msg *RealmLogin) Deserialize(in io.Reader) error {
+func (msg *RealmLoginReq) Deserialize(in io.Reader) error {
 	fmt.Fscanf(in, "AT%s", &msg.Ticket)
 	return nil
 }
