2fd0c489dca3d76a42ced0e70129ad7cddd412bb
Blackrush <blackrushx@gmail.com>
2013-09-05 17:48:10 +0200

    adds new messages

--- protocol/frontend/msg/msg.go
+++ protocol/frontend/msg/msg.go
@@ -2,6 +2,7 @@ package msg
 
 import (
 	protocol "github.com/Blackrush/gofus/protocol/frontend"
+	"strconv"
 )
 
 // Converts a boolean value to an integer (1 if true or 0 if false)
@@ -10,6 +11,25 @@ func btoi(b bool) int {
 		return 1
 	}
 	return 0
+}
+
+// Converts an integer value to a boolean (true if 1, false otherwise)
+func itob(i int) bool {
+	return i == 1
+}
+
+// Converts an integer represented as a string to a boolean (true if "1", false otherwise)
+func aitob(str string) bool {
+	return str == "1"
+}
+
+// Converts a string to an integer
+func atoi(str string) int {
+	if result, err := strconv.Atoi(str); err == nil {
+		return result
+	} else {
+		panic(err.Error())
+	}
 }
 
 var producers = make(map[string]func() protocol.MessageContainer)
@@ -23,7 +43,10 @@ func init() {
 	producers["AV"] = func() protocol.MessageContainer { return new(RegionalVersionReq) }
 	producers["Ag"] = func() protocol.MessageContainer { return new(PlayersGiftsReq) }
 	producers["AG"] = func() protocol.MessageContainer { return new(SetPlayerGiftReq) }
+	producers["Ai"] = func() protocol.MessageContainer { return new(SetIdentity) }
 	producers["AL"] = func() protocol.MessageContainer { return new(PlayersReq) }
+	producers["AP"] = func() protocol.MessageContainer { return new(RandNameReq) }
+	producers["AA"] = func() protocol.MessageContainer { return new(CreateUserReq) }
 }
 
 func New(opcode string) (protocol.MessageContainer, bool) {
--- protocol/frontend/msg/realm.go
+++ protocol/frontend/msg/realm.go
@@ -2,7 +2,10 @@ package msg
 
 import (
 	"fmt"
+	"github.com/Blackrush/gofus/realm/db"
 	"io"
+	"strings"
+	"time"
 )
 
 type HelloGame struct{}
@@ -102,8 +105,101 @@ func (msg *SetPlayerGiftReq) Deserialize(in io.Reader) error {
 	return nil
 }
 
+type SetIdentity struct {
+	Identity string
+}
+
+func (msg *SetIdentity) Opcode() string { return "Ai" }
+func (msg *SetIdentity) Serialize(out io.Writer) error {
+	fmt.Fprint(out, msg.Identity)
+	return nil
+}
+func (msg *SetIdentity) Deserialize(in io.Reader) error {
+	fmt.Fscanf(in, "Ai%s", &msg.Identity)
+	return nil
+}
+
 type PlayersReq struct{}
 
 func (msg *PlayersReq) Opcode() string                 { return "AL" }
 func (msg *PlayersReq) Serialize(out io.Writer) error  { return nil }
 func (msg *PlayersReq) Deserialize(in io.Reader) error { return nil }
+
+type PlayersResp struct {
+	ServerId        uint
+	SubscriptionEnd time.Time
+	Players         []*db.Player
+}
+
+func (msg *PlayersResp) Opcode() string { return "ALK" }
+func (msg *PlayersResp) Serialize(out io.Writer) error {
+	fmt.Fprintf(out, "%d|%d", msg.SubscriptionEnd.Sub(time.Now()).Nanoseconds()/1e6, len(msg.Players))
+	for _, player := range msg.Players {
+		fmt.Fprintf(out, "|%d;%s;%d;%d;%d;%d;%d;%s;%d;;;",
+			player.Id,
+			player.Name,
+			player.Experience.Level,
+			player.Appearance.Skin,
+			player.Appearance.Colors.First,
+			player.Appearance.Colors.Second,
+			player.Appearance.Colors.Third,
+			player.Appearance.Accessories,
+			msg.ServerId,
+		)
+	}
+	return nil
+}
+func (msg *PlayersResp) Deserialize(in io.Reader) error {
+	return nil
+}
+
+type RandNameReq struct{}
+
+func (msg *RandNameReq) Opcode() string                 { return "AP" }
+func (msg *RandNameReq) Serialize(out io.Writer) error  { return nil }
+func (msg *RandNameReq) Deserialize(in io.Reader) error { return nil }
+
+type RandNameResp struct {
+	Name string
+}
+
+func (msg *RandNameResp) Opcode() string { return "AP" }
+func (msg *RandNameResp) Serialize(out io.Writer) error {
+	fmt.Fprint(out, msg.Name)
+	return nil
+}
+func (msg *RandNameResp) Deserialize(in io.Reader) error {
+	return nil
+}
+
+type CreateUserReq struct {
+	Name   string
+	Breed  int
+	Gender bool
+	Colors struct {
+		First  int
+		Second int
+		Third  int
+	}
+}
+
+func (msg *CreateUserReq) Opcode() string { return "AA" }
+func (msg *CreateUserReq) Serialize(out io.Writer) error {
+	fmt.Fprintf(out, "%s|%d|%d|%d|%d|%d", msg.Name, msg.Breed, btoi(msg.Gender), msg.Colors.First, msg.Colors.Second, msg.Colors.Third)
+	return nil
+}
+func (msg *CreateUserReq) Deserialize(in io.Reader) error {
+	var body string
+	fmt.Fscanf(in, "AA%s", &body)
+
+	args := strings.SplitN(body, "|", 6)
+
+	msg.Name = args[0]
+	msg.Breed = atoi(args[1])
+	msg.Gender = aitob(args[2])
+	msg.Colors.First = atoi(args[3])
+	msg.Colors.Second = atoi(args[4])
+	msg.Colors.Third = atoi(args[5])
+
+	return nil
+}
