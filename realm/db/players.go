9628b87227c07f3ead04766bce53e7af0e1d94c8
Blackrush <blackrushx@gmail.com>
2013-10-03 18:24:42 +0200

    adds players crud

--- realm/db/players.go
+++ realm/db/players.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 )
 
 type PlayerExperience struct {
@@ -81,6 +82,8 @@ type Player struct {
 	Name       string
 	Appearance PlayerAppearance
 	Experience PlayerExperience
+
+	persisted bool
 }
 
 type Players struct {
@@ -139,4 +142,117 @@ func (p *Players) rem_player(player *Player) {
 	if players, ok := p.players_by_owner_id[player.OwnerId]; ok {
 		players_remove(&players, player)
 	}
+}
+
+func player_values(player *Player, with_id, id_last bool) []interface{} {
+	result := []interface{}
+	if with_id && !id_last {
+		result = append(result, player.Id)
+	}
+	result = append(result,
+		player.Id,
+		player.OwnerId,
+		player.Name,
+		player.Appearance.Skin,
+		player.Appearance.Colors.First,
+		player.Appearance.Colors.Second,
+		player.Appearance.Colors.Third,
+		player.Experience.Level,
+		player.Experience.Experience,
+	)
+	if with_id && id_last {
+		result = append(result, player.Id)
+	}
+	return result
+}
+
+func player_ptrvalues(player *Player) []interface{} {
+	return []interface{}{
+		&player.Id,
+		&player.OwnerId,
+		&player.Name,
+		&player.Appearance.Skin,
+		&player.Appearance.Colors.First,
+		&player.Appearance.Colors.Second,
+		&player.Appearance.Colors.Third,
+		&player.Experience.Level,
+		&player.Experience.Experience,
+	}
+}
+
+func (p *Players) FindAll() ([]*Player, bool) {
+	rows, err := p.db.Query("select id, owner_id, name, skin, first_color, second_color, third_color, level, experience from players")
+	if err != nil {
+		log.Print(err)
+		return nil, false
+	}
+
+	result := []*Player{}
+	for rows.Next() {
+		player := &Player{persisted: true}
+		if err := rows.Scan(player_ptrvalues(player)); err != nil {
+			log.Print(err)
+			return nil, false
+		}
+		result = append(result, player)
+	}
+
+	return result, true
+}
+
+func (p *Players) Persist(player *Player) (inserted bool, success bool) {
+	if !player.persisted {
+		stmt, err := p.db.Prepare("insert into players(owner_id, name, skin, first_color, second_color, third_color, level, experience) values($1, $2, $3, $4, $5, $6, $7, $8")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		res, err := stmt.Exec(player_values(player, false, false))
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		if id, err := res.LastInsertId(); err == nil {
+			player.Id = id
+		} else {
+			log.Print(err)
+			return
+		}
+
+		player.persisted = true
+		inserted = true
+	} else {
+		stmt, err := p.db.Prepare("update players set owner_id=$1, name=$2, skin=$3, first_color=$4, second_color=$5, third_color=$6, level=$7, experience=$8 where id=$9")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		if _, err := stmt.Exec(player_values(player, true, true)); err != nil {
+			log.Print(err)
+			return
+		}
+	}
+
+	success = true
+	return
+}
+
+func (p *Players) Remove(player *Player) (success bool) {
+	if !player.persisted {
+		return false
+	}
+
+	if stmt, err := p.db.Prepare("delete from players where id=$1"); err == nil {
+		if _, err := stmt.Exec(player.Id); err == nil {
+			return true
+		} else {
+			log.Print(err)
+		}
+	} else {
+		log.Print(err)
+	}
+	return false
 }
