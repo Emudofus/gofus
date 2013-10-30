6dcbdbcabd7e30744111b4c061613a63ca328cd5
Blackrush <blackrushx@gmail.com>
2013-10-30 17:17:59 +0100

    moves some handler in another file

--- realm/network/frontend/handler/handler.go
+++ realm/network/frontend/handler/handler.go
@@ -8,20 +8,6 @@ import (
 	"log"
 )
 
-func client_send_player_list(ctx frontend.Service, client frontend.Client) {
-	var players []*db.Player
-	var ok bool
-	if players, ok = ctx.Players().GetByOwnerId(uint(client.UserInfos().Id)); !ok {
-		players = make([]*db.Player, 0, 0)
-	}
-
-	client.Send(&msg.PlayersResp{
-		ServerId:        ctx.Config().ServerId,
-		SubscriptionEnd: client.UserInfos().SubscriptionEnd,
-		Players:         players,
-	})
-}
-
 func HandleClientConnection(ctx frontend.Service, client frontend.Client) {
 	log.Printf("[frontend-net-client-%04d] CONN", client.Id())
 
@@ -55,50 +41,13 @@ func HandleClientData(ctx frontend.Service, client frontend.Client, arg protocol
 	}
 }
 
-func handle_client_login(ctx frontend.Service, client frontend.Client, m *msg.RealmLoginReq) {
-	if infos, ok := ctx.Backend().GetUserInfos(m.Ticket); ok {
-		client.SetUserInfos(*infos)
-		ctx.Backend().NotifyUserConnection(infos.Id, true)
-
-		client.Send(&msg.RealmLoginSuccess{ctx.Config().CommunityId})
-	} else {
-		client.CloseWith(&msg.RealmLoginError{})
-	}
-}
-
-func handle_client_regional_version(ctx frontend.Service, client frontend.Client, m *msg.RegionalVersionReq) {
-	client.Send(&msg.RegionalVersionResp{ctx.Config().CommunityId})
-}
-
-func handle_client_players(ctx frontend.Service, client frontend.Client, m *msg.PlayersReq) {
-	client_send_player_list(ctx, client)
-}
-
-func handle_client_rand_name(ctx frontend.Service, client frontend.Client, m *msg.RandNameReq) {
-	client.Send(&msg.RandNameResp{Name: "Gofus"})
-}
-
-func handle_client_create_player(ctx frontend.Service, client frontend.Client, m *msg.CreatePlayerReq) {
-	player := ctx.Players().NewPlayer(uint(client.UserInfos().Id), m.Name, m.Breed, m.Gender, m.Colors.First, m.Colors.Second, m.Colors.Third)
-
-	if inserted, success := ctx.Players().Persist(player); inserted && success {
-		client_send_player_list(ctx, client)
-	} else {
-		client.Send(&msg.CreatePlayerErrorResp{})
-	}
-}
-
-func handle_client_player_selection(ctx frontend.Service, client frontend.Client, m *msg.PlayerSelectionReq) {
-	player, ok := ctx.Players().GetById(m.PlayerId)
-	if !ok {
-		client.CloseWith(&msg.PlayerSelectionErrorResp{})
-		return
-	}
-
-	client.SetPlayer(player)
-	client.Send(&msg.PlayerSelectionResp{player})
-}
-
 func handle_client_game_context_create(ctx frontend.Service, client frontend.Client, m *msg.GameContextCreateReq) {
-	log.Print("YOSH")
+	switch m.Type {
+	case msg.SoloContextType:
+
+	case msg.FightContextType:
+		fallthrough
+	default:
+		log.Print("context type %d is not implemented yet", m.Type)
+	}
 }
---
+++ realm/network/frontend/handler/realm.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"github.com/Blackrush/gofus/protocol/frontend/msg"
+	"github.com/Blackrush/gofus/realm/network/frontend"
+)
+
+func client_send_player_list(ctx frontend.Service, client frontend.Client) {
+	var players []*db.Player
+	var ok bool
+	if players, ok = ctx.Players().GetByOwnerId(uint(client.UserInfos().Id)); !ok {
+		players = make([]*db.Player, 0, 0)
+	}
+
+	client.Send(&msg.PlayersResp{
+		ServerId:        ctx.Config().ServerId,
+		SubscriptionEnd: client.UserInfos().SubscriptionEnd,
+		Players:         players,
+	})
+}
+
+func handle_client_login(ctx frontend.Service, client frontend.Client, m *msg.RealmLoginReq) {
+	if infos, ok := ctx.Backend().GetUserInfos(m.Ticket); ok {
+		client.SetUserInfos(*infos)
+		ctx.Backend().NotifyUserConnection(infos.Id, true)
+
+		client.Send(&msg.RealmLoginSuccess{ctx.Config().CommunityId})
+	} else {
+		client.CloseWith(&msg.RealmLoginError{})
+	}
+}
+
+func handle_client_regional_version(ctx frontend.Service, client frontend.Client, m *msg.RegionalVersionReq) {
+	client.Send(&msg.RegionalVersionResp{ctx.Config().CommunityId})
+}
+
+func handle_client_players(ctx frontend.Service, client frontend.Client, m *msg.PlayersReq) {
+	client_send_player_list(ctx, client)
+}
+
+func handle_client_rand_name(ctx frontend.Service, client frontend.Client, m *msg.RandNameReq) {
+	client.Send(&msg.RandNameResp{Name: "Gofus"})
+}
+
+func handle_client_create_player(ctx frontend.Service, client frontend.Client, m *msg.CreatePlayerReq) {
+	player := ctx.Players().NewPlayer(uint(client.UserInfos().Id), m.Name, m.Breed, m.Gender, m.Colors.First, m.Colors.Second, m.Colors.Third)
+
+	if inserted, success := ctx.Players().Persist(player); inserted && success {
+		client_send_player_list(ctx, client)
+	} else {
+		client.Send(&msg.CreatePlayerErrorResp{})
+	}
+}
+
+func handle_client_player_selection(ctx frontend.Service, client frontend.Client, m *msg.PlayerSelectionReq) {
+	player, ok := ctx.Players().GetById(m.PlayerId)
+	if !ok {
+		client.CloseWith(&msg.PlayerSelectionErrorResp{})
+		return
+	}
+
+	client.SetPlayer(player)
+	client.Send(&msg.PlayerSelectionResp{player})
+}
